4f7f434481a68bc06908a2a72410d2d2030ecac5
AnhSP <chimpui101@gmail.com>
2023-10-24 14:31:11 +0700

    up tiep index part 2, them cac icon

--- Template/web/MyJavaScript/myscript.js
+++ Template/web/MyJavaScript/myscript.js
@@ -1,1 +1,9 @@
-
+window.addEventListener("load", function(){
+    $('.datepicker').datepicker({
+        autoclose: true, 
+        todayHighlight: true,
+        todayBtn : "linked",
+        title : "Chon ngày tháng năm",
+        format: 'dd/mm/yyyy'
+    });
+})
