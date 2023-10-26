42f73bc98fe9621f9480af45cc9caced1ed1efa9
AnhSP <chimpui101@gmail.com>
2023-10-26 10:47:23 +0700

    hieu ung lam noi 3D, them homestay, fix home html

---
+++ Template/homestay/MyJavaScript/myscript.js
@@ -0,0 +1,14 @@
+// Sử dụng jquery để Làm nổi 3D các khách sạn
+// $(document).ready(function() {
+//     // Bắt sự kiện hover khi con chuột di chuyển vào hoặc ra khỏi .hotel-card
+//     $('.hotel-card').hover(function() {
+//         // Hover vào
+//         $(this).css('transform', 'translateY(-20px)');
+//         $(this).css('background-color', '#ccc');
+//     }, function() {
+//         // Hover ra
+//         $(this).css('transform', 'translateY(0)');
+//         $(this).css('background-color', 'transparent');
+//     });
+// });
+// Không hiểu tại sao không có Sử dụng jquery để Làm nổi 3D các khách sạn mà nó vẫn hoạt động bình thường
