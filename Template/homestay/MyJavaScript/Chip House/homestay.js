0faaba3325d4949c47d1ae7beb5c911db7b90038
AnhSP <chimpui101@gmail.com>
2023-10-28 19:45:28 +0700

    them toan bo cau truc folder + file

---
+++ Template/homestay/MyJavaScript/Chip House/homestay.js
@@ -0,0 +1,18 @@
+// Kiểm tra đã login chưa
+function checkBook1(){
+    var isLogined = localStorage.getItem("userLoggedIn")
+    if(isLogined==="true"){
+        window.location.href = "/WebApp/Views/Dat Phong/Homestay/An House/datphong1.html";
+    }else{
+        alert("Bạn cần đăng nhập để có thể đặt phòng");
+    }
+}
+
+function checkBook2(){
+    var isLogined = localStorage.getItem("userLoggedIn")
+    if(isLogined==="true"){
+        window.location.href = "/WebApp/Views/Dat Phong/Homestay/An House/datphong2.html";
+    }else{
+        alert("Bạn cần đăng nhập để có thể đặt phòng");
+    }
+}
