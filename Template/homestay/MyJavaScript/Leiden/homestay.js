5b46d40f92154c7b6b072e61d14ad0e902f19876
AnhSP <chimpui101@gmail.com>
2023-12-15 16:12:34 +0700

    fix

--- Template/homestay/MyJavaScript/Leiden/homestay.js
+++ Template/homestay/MyJavaScript/Leiden/homestay.js
@@ -2,16 +2,16 @@
 function checkBook1(){
     var isLogined = localStorage.getItem("userLoggedIn")
     if(isLogined==="true"){
-        window.location.href = "/WebApp/Views/Dat Phong/Homestay/An House/datphong1.html";
+        window.location.href = "/WebApp/Views/Dat Phong/Homestay/Leiden/datphong1.html";
     }else{
         alert("Bạn cần đăng nhập để có thể đặt phòng");
     }
 }
 
-function checkBook1(){
+function checkBook2(){
     var isLogined = localStorage.getItem("userLoggedIn")
     if(isLogined==="true"){
-        window.location.href = "/WebApp/Views/Dat Phong/Homestay/An House/datphong2.html";
+        window.location.href = "/WebApp/Views/Dat Phong/Homestay/Leiden/datphong2.html";
     }else{
         alert("Bạn cần đăng nhập để có thể đặt phòng");
     }
